c60bbb67d9032e38200a3659b4f1ad3baf320f90
stojan97 <stojan.samojlovski@gmail.com>
2023-12-02 23:03:23 +0100

    day1 solution

--- day-1/solution.py
+++ day-1/solution.py
@@ -1,17 +1,41 @@
-def get_input():
-    with open('sample') as file:
+def get_input() -> list[str]:
+    with open('input') as file:
         lines = [line.strip() for line in file]
 
     print(lines)
     return lines
 
 
-def part1(input):
-    pass
+def get_next_digit_p1(a, rev):
+    return next(filter(lambda s: s.isdigit(), a))
 
 
-def part2(input):
-    pass
+def get_next_digit_p2(a, rev):
+    digits = [(3, {'one': '1', 'two': '2', 'six': '6'}), (4, {'four': '4', 'five': '5', 'nine': '9'}), (5, {'three': '3', 'seven': '7', 'eight': '8'})]
+
+    for i in range(len(a)):
+        if a[i].isdigit():
+            return a[i]
+
+        for x, m in digits:
+            c = a[i: i + x]
+            cut = c if not rev else c[::-1]
+            if cut in m:
+                return m[cut]
+
+    return None
+
+
+def solve(document, get_next_digit):
+    return sum(int(get_next_digit(value, False) + get_next_digit(value[::-1], True)) for value in document)
+
+
+def part1(document):
+    return solve(document, get_next_digit_p1)
+
+
+def part2(document):
+    return solve(document, get_next_digit_p2)
 
 
 inp = get_input()
