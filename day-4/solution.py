e7c2c4c85c2cd1720fc59c53725f838f2b245f14
stojan97 <stojan.samojlovski@gmail.com>
2023-12-04 18:21:53 +0100

    day4 solution

---
+++ day-4/solution.py
@@ -0,0 +1,45 @@
+import re
+
+
+def get_input():
+    lines = []
+    with open('input') as file:
+        for line in file:
+            line = line.strip()
+            winning, your_card = line.split(':')[1].split('|')
+            winning = set(map(int, re.findall(r'\d+', winning.strip())))
+            your_card = set(map(int, re.findall(r'\d+', your_card.strip())))
+            lines.append((winning, your_card))
+
+    return lines
+
+
+def get_matching(cards):
+    card_score = []
+
+    for i, (winning, your_card) in enumerate(cards):
+        res = winning.intersection(your_card)
+        matching = len(res)
+        card_score.append(matching)
+
+    return card_score
+
+
+def part1(cards):
+    return sum(int(2 ** (matching - 1)) for matching in get_matching(cards))
+
+
+def part2(cards):
+    matching = get_matching(cards)
+    card_count = [1] * len(cards)
+
+    for i in range(len(cards)):
+        for j in range(i + 1, i + matching[i] + 1):
+            card_count[j] += card_count[i]
+
+    return sum(card_count)
+
+
+inp = get_input()
+print('Part 1:', part1(inp))
+print('Part 2:', part2(inp))
