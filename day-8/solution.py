80a992eaad39939e27813bbfae89f281cc5b1f3b
stojan97 <stojan.samojlovski@gmail.com>
2023-12-09 00:40:50 +0100

    day8 solution

---
+++ day-8/solution.py
@@ -0,0 +1,58 @@
+import re
+from collections import deque
+from math import lcm
+
+
+def get_input():
+    lines = []
+    with open('input') as file:
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            lines.append(line)
+
+    edges = {}
+
+    ins_mapper = {'L': 0, 'R': 1}
+
+    instructions = [ins_mapper[ins] for ins in lines[0]]
+
+    for l in lines[1:]:
+        edge = re.findall(r'\w+', l)
+        edges[edge[0]] = [edge[1], edge[2]]
+
+    return instructions, edges
+
+
+def get_steps_for_node(node, instructions, edges):
+    i, s = 0, 0
+    while not node.endswith('Z'):
+        node = edges[node][instructions[i]]
+        i = (i + 1) % len(instructions)
+        s += 1
+
+    return s
+
+
+def part1(input):
+    instructions, edges = input
+    s = get_steps_for_node('AAA', instructions, edges)
+    return s
+
+
+def part2(input):
+    instructions, edges = input
+    all_nodes = [k for k in edges.keys() if k.endswith('A')]
+    res = 1
+
+    for node in all_nodes:
+        steps = get_steps_for_node(node, instructions, edges)
+        res = lcm(res, steps)
+
+    return res
+
+
+inp = get_input()
+print('Part 1:', part1(inp))
+print('Part 2:', part2(inp))
